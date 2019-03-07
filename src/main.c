b4931057ef378e08c323224fa71e89cce3005cda
Sleepy105 <lm.sousa@fe.up.pt>
2019-03-07 15:01:23 +0000

    Import configuration from JSON file

--- src/main.c
+++ src/main.c
@@ -21,9 +21,15 @@ int main(int argc, char const *argv[])
         Room* room = elem->ptr;
         printf("%d,%s\n", room->id, room->name);
 
+        // Print node data
         for (list_element* elem2 = listStart(room->nodes); elem2 != NULL; elem2 = elem2->next) {
             Node* node = elem2->ptr;
             printf("  %d\n", node->id);
+
+            for (list_element* elem3 = listStart(node->sensors); elem3 != NULL; elem3 = elem3->next) {
+                Sensor* sensor = elem3->ptr;
+                printf("    t: %d\n", sensor->type);
+            }
         }
     }
 
