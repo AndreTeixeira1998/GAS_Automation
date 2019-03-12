d812aae6663c3e3a8cc9f20cd3ea0841ede64c75
Sleepy105 <lm.sousa@fe.up.pt>
2019-03-12 22:19:53 +0000

    remove test code from main.c

--- src/main.c
+++ src/main.c
@@ -12,28 +12,8 @@ int main(int argc, char const *argv[])
         return 1;
     }
     
-    for (list_element* elem = listStart(datastore->rooms); elem != NULL; elem = elem->next) {
-        Room* room = elem->ptr;
-        printf("%d,%s\n", room->id, room->name);
-
-        // Print node data
-        for (list_element* elem2 = listStart(room->nodes); elem2 != NULL; elem2 = elem2->next) {
-            Node* node = elem2->ptr;
-            printf("  %d\n", node->id);
-
-            for (list_element* elem3 = listStart(node->sensors); elem3 != NULL; elem3 = elem3->next) {
-                Sensor* sensor = elem3->ptr;
-                printf("    s_t: %d\n", sensor->type);
-            }
-
-            for (list_element* elem3 = listStart(node->actuators); elem3 != NULL; elem3 = elem3->next) {
-                Actuator* actuator = elem3->ptr;
-                Position* pos = getActuatorPosition(actuator);
-                printf("    a_x: %d | a_y: %d\n", pos->x, pos->y);
-            }
-        }
-    }
-
+    
+    
     printf("Done\n");
     
     return 0;
