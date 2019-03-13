5e823d83545905f8f5944825a519dc872d63a052
Sleepy105 <lm.sousa@fe.up.pt>
2019-03-13 23:05:16 +0000

    Add minor code documentation

--- src/main.c
+++ src/main.c
@@ -48,24 +48,30 @@ int main(int argc, char const *argv[])
     int thread_IDs[3];
     void* thread_retValues[3];
     
+    // Create the threads
     thread_IDs[THREAD_READINPUT] = pthread_create(&threads[THREAD_READINPUT], NULL, &readInput, datastore);
     thread_IDs[THREAD_EXECUTERULES] = pthread_create(&threads[THREAD_EXECUTERULES], NULL, &executeRules, datastore);
     thread_IDs[THREAD_WRITEOUTPUT] = pthread_create(&threads[THREAD_WRITEOUTPUT], NULL, &writeOutput, datastore);
 
+    // Wait for thread endings
     pthread_join(threads[THREAD_READINPUT], &thread_retValues[THREAD_READINPUT]);
     pthread_join(threads[THREAD_EXECUTERULES], &thread_retValues[THREAD_EXECUTERULES]);
     pthread_join(threads[THREAD_WRITEOUTPUT], &thread_retValues[THREAD_WRITEOUTPUT]);
 
+    // Print Thread return values
     printf("%d\n", *(int*)thread_retValues[THREAD_READINPUT]);
     printf("%d\n", *(int*)thread_retValues[THREAD_EXECUTERULES]);
     printf("%d\n", *(int*)thread_retValues[THREAD_WRITEOUTPUT]);
 
+    // Free memory used by threads to return a value to the parent process
     uint8_t nThreads = sizeof(thread_retValues)/sizeof(thread_retValues[0]);
     for (int i = 0; i < nThreads; i++) {
         free(thread_retValues[i]);
     }
 
+    // Free DataStructure data
     deleteDatastore(datastore);
+    
     printf("Done\n");
     
     return 0;
