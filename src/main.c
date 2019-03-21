1c46f85a81152ad792e253d9a3024b657fde0d86
Sleepy105 <lm.sousa@fe.up.pt>
2019-03-21 14:45:09 +0000

    Revert main

--- src/main.c
+++ src/main.c
@@ -7,10 +7,10 @@
 
 int main(int argc, char const *argv[])
 {
-    /*if (argc < 4) {
+    if (argc < 4) {
         printf("Not enough arguments. Expecting:\n\t%s <configuration-file> <input-stream> <output-stream>\n\n", argv[0]);
         return 1;
-    }*/
+    }
 
     Datastore* datastore = importConfiguration(argv[1]);
     if (!datastore) {
@@ -18,19 +18,7 @@ int main(int argc, char const *argv[])
         return 1;
     }
 
-    LL_iterator(datastore->rooms, room_elem) {
-        Room* room = room_elem->ptr;
-        LL_iterator(room->rules, rule_elem) {
-            Rule* rule = rule_elem->ptr;
-            printf("RULE\n");
-            LL_iterator(rule->sensors, sensor_elem) {
-                Sensor* sensor = sensor_elem->ptr;
-                printf("\t%d\n", sensor->type);
-            }
-        }
-    }
-    
-    /*
+
     FILE* inputStream = fopen(argv[2], "r");
     FILE* outputStream = fopen(argv[3], "w");
     if (!inputStream || !outputStream) {
@@ -43,7 +31,7 @@ int main(int argc, char const *argv[])
     printf("Done\n");
 
     fclose(inputStream);
-    fclose(outputStream);*/
+    fclose(outputStream);
     deleteDatastore(datastore);
     return 0;
 }
