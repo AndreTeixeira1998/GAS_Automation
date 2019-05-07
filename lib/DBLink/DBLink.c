8fab440cb90b63888a0eff4597577b35506c0085
Sleepy105 <lm.sousa@fe.up.pt>
2019-05-07 15:43:26 +0100

    Move queryArray to DBLink.h

--- lib/DBLink/DBLink.c
+++ lib/DBLink/DBLink.c
@@ -64,13 +64,6 @@ char* getConnectionStringFromFile (const char* filename) {
     return str;
 }
 
-typedef struct {
-    PGconn* conn;
-    char* name;
-    char* query;
-    int nParams;
-} DBQuery;
-
 void __DB_prepareQuery (DBQuery query) {
     
     PGresult* stmt = PQprepare(
@@ -85,38 +78,16 @@ void __DB_prepareQuery (DBQuery query) {
 
 void DB_prepareAllSQLQueries (PGconn* conn) {
     if (!conn || PQstatus(conn) != CONNECTION_OK) {
-        return true;
+        return;
     }
 
-    int nQueries = 2; // Change to match number of nodes below
-
-    DBQuery nodes[2] = {
-        // {
-        //     connection_to_db,
-        //     query_name,
-        //     query_part_1
-        //     query_part_2,
-        //     0
-        // },
-        {
-            conn,
-            "create_node",
-            "INSERT INTO sinf.node "
-            "DEFAULT VALUES;",
-            0
-        },
-        {
-            conn,
-            "create_actuator",
-            "INSERT INTO sinf.actuator(type, pixel_id) "
-            "VALUES($1, $2);",
-            2
-        }
-    };
-
-    for (int i = 0; i < nQueries; i++) {
-        __DB_prepareQuery(nodes[i]);
+    for (int i = 0; i < queryArraySize; i++) {
+        __DB_prepareQuery(queryArray[i]);
     }
+}
+
+void DB_createNode () {
+
 }
 
 Datastore* DB_importConfiguration (PGconn* conn) {
--- lib/DBLink/DBLink.h
+++ lib/DBLink/DBLink.h
@@ -4,8 +4,42 @@
 #include <libpq-fe.h>
 #include "DataStructure.h"
 
+typedef struct {
+    PGconn* conn;
+    char* name;
+    char* query;
+    int nParams;
+} DBQuery;
+
 char* getConnectionStringFromFile (const char* filename);
 void DB_prepareAllSQLQueries (PGconn* conn);
 Datastore* DB_importConfiguration (PGconn* conn);
+
+/////////////////////////////////
+/////// QUERY DEFENITIONS ///////
+/////////////////////////////////
+
+#define queryArraySize 2 // Change to match number of nodes below
+
+DBQuery* queryArray[2] = {
+    &create_node,
+    &create_actuator
+};
+
+DBQuery create_node = {
+    conn,
+    "create_node",
+    "INSERT INTO sinf.node "
+    "DEFAULT VALUES;",
+    0
+}
+
+DBQuery create_node = {
+    conn,
+    "create_actuator",
+    "INSERT INTO sinf.actuator(type, pixel_id) "
+    "VALUES($1, $2);",
+    2
+}
 
 #endif
