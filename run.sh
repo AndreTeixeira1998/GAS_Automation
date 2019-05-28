4d961a80d163b17e61e32fecb9d487c72523fced
Sleepy105 <lm.sousa@fe.up.pt>
2019-05-28 10:16:48 +0100

    run valgrind without sockets

--- run.sh
+++ run.sh
@@ -1,30 +1,30 @@
-#Setup sockets
-socat -d -d pty,raw,echo=0 pty,raw,echo=0 &
-P1=$!
-sleep 0.25
-read -p "Enter first socat in: " firstin
-read -p "Enter first socat out: " firstout
-
-socat -d -d pty,raw,echo=0 pty,raw,echo=0 &
-P2=$!
-sleep 0.25
-read -p "Enter second socat in: " secondin
-read -p "Enter second socat out: " secondout
-
-#Start Simulators
-echo $PWD &
-python $PWD/sims/MsgCreator.py > /dev/pts/$firstin &
-P3=$!
-
-python $PWD/sims/RGBMatrix.py < /dev/pts/$secondout &
-P4=$!
-
-sleep 3
-
 if getopts v: option
 then
-    bash -c "valgrind --leak-check=full ./GAS.out GASconfig.json connString.conf /dev/pts/$firstout /dev/pts/$secondin"
+    bash -c "valgrind --leak-check=full ./GAS.out GASconfig.json connString.conf /dev/null /dev/null"
 else
+    #Setup sockets
+    socat -d -d pty,raw,echo=0 pty,raw,echo=0 &
+    P1=$!
+    sleep 0.25
+    read -p "Enter first socat in: " firstin
+    read -p "Enter first socat out: " firstout
+
+    socat -d -d pty,raw,echo=0 pty,raw,echo=0 &
+    P2=$!
+    sleep 0.25
+    read -p "Enter second socat in: " secondin
+    read -p "Enter second socat out: " secondout
+
+    #Start Simulators
+    echo $PWD &
+    python $PWD/sims/MsgCreator.py > /dev/pts/$firstin &
+    P3=$!
+
+    python $PWD/sims/RGBMatrix.py < /dev/pts/$secondout &
+    P4=$!
+
+    sleep 3
+
     bash -c "./GAS.out GASconfig.json connString.conf /dev/pts/$firstout /dev/pts/$secondin"
 fi
 
